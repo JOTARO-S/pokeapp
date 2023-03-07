02aec8b649f6315f765e18c7f84b6999742dbe3d
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2023-03-07 13:42:07 +0000

    pokemon

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -5,7 +5,7 @@ class PokemonsController < ApplicationController
     if raw_response.status == 200
       @response = JSON.parse(raw_response.body)
     else
-      redirect_to new_pokemon_path, notice: "#{raw_response.status}エラー！"
+      redirect_to root_path, notice: "#{raw_response.status}エラー！"
     end
   end
 end
